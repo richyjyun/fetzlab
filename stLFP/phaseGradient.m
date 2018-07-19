f0d2c7d689cb5bd17aa3e1b26df2854827a3ad90
richy.yun <richy.yun@FA-Temp-I521.wanprc.org>
2018-07-18 18:47:30 -0700

    Comments on phase gradient code

--- stLFP/phaseGradient.m
+++ stLFP/phaseGradient.m
@@ -1,56 +1,69 @@
 function [fx,fy] = phaseGradient(phase,k)
+% Summary
+%   Calculates the phase gradient of the grid of phases. ASSUMES USE WITH
+%   IMAGESC (FLIPPED YDIR)
+%
+% Inputs
+%   phase       Grid of phase values (-pi to pi)
+%   k           Distance of neighbors to use for calculation
+%
+% Outputs
+%   fx          Real part of phase gradient (x component. For quiver)
+%   fy          Imaginary part of phase gradient (y component)
+%
+% RJY 07//18/2018
 
-    if ~exist('k','var')
-        k = 2;
-    end
+if ~exist('k','var')
+    k = 2;
+end
 
-    gradient = nan(size(phase));
+gradient = nan(size(phase));
+
+for y = 1:size(phase,1)
     
-    for y = 1:size(phase,1)
+    for x = 1:size(phase,2)
         
-        for x = 1:size(phase,2)
+        if(isnan(phase(y,x))), continue; end
+        
+        gradient(y,x) = 0;
+        
+        for i = -k:k
             
-            if(isnan(phase(y,x))), continue; end
-            
-            gradient(y,x) = 0;
-            
-            for i = -k:k
-                              
-                for j = -k:k
-                    
-                    if(i==0 && j==0), continue; end
-                    
-                    if(x+i<=0 || x+i>size(phase,2) || y+j<=0 || y+j>size(phase,1)), continue; end
-                    
-                    if(isnan(phase(y+j,x+i))), continue; end
-                    
-                    p = phase(y+j,x+i)-phase(y,x);
-                    
-                    if(sign(p))
-                        div = -pi;
-                    else
-                        div = pi;
-                    end
-                    
-                    p = mod(p,div);
-                    
-                    d = sqrt(i^2+j^2);
-                    
-                    a = atan2(-j,i); % need negative j just because of how rows/columns are defined 
-
-                    gradient(y,x) = gradient(y,x) + (p/d)*exp(1j*a);
-                                        
+            for j = -k:k
+                
+                if(i==0 && j==0), continue; end
+                
+                if(x+i<=0 || x+i>size(phase,2) || y+j<=0 || y+j>size(phase,1)), continue; end
+                
+                if(isnan(phase(y+j,x+i))), continue; end
+                
+                p = phase(y+j,x+i)-phase(y,x);
+                
+                if(sign(p))
+                    div = -pi;
+                else
+                    div = pi;
                 end
                 
+                p = mod(p,div);
+                
+                d = sqrt(i^2+j^2);
+                
+                a = atan2(-j,i); % need negative j just because of how rows/columns are defined
+                
+                gradient(y,x) = gradient(y,x) + (p/d)*exp(1j*a);
+                
             end
-                        
+            
         end
         
     end
     
-    fx = real(gradient);
-    fy = -imag(gradient); % need to be negative to make it display properly (imagesc flips yaxis)
-    
+end
+
+fx = real(gradient);
+fy = -imag(gradient); % need to be negative to make it display properly (imagesc flips yaxis)
+
 end
 
 
---
+++ stLFP/plotPhaseGradient.m
@@ -0,0 +1,71 @@
+%% For plotting the phase gradient
+% Loads in data, filters to beta, calculates instantaneous phase, loops to 
+% find gradient at each time step. 
+
+clear; close all;
+
+%% Load in proper times
+tankpath = 'Y:\~NeuroWest\Spanky\RandomStim-180314-124242\';
+% tankpath = 'Y:\~NeuroWest\Spanky\SpikeTrigger-180122-105223\';
+% tankpath = 'Y:\~NeuroWest\Spanky\IFNN\';
+% tankpath = 'Y:\~NeuroWest\Spanky\CycleTriggered-170710-143939\';
+% tankpath = 'Y:\~NeuroWest\Spanky\Connectivity-180207-131758\';
+blockname = 'Spanky-180717-135403';
+times = [0,10*60];
+
+T1 = times(1); T2 = times(2);
+LFPs = TDT2mat([tankpath,blockname],'T1',T1,'T2',T2,'TYPE',4,'STORE','LFPs'); LFPs = LFPs.streams.LFPs;
+fs = LFPs.fs;
+
+LFPs.data = bpfilt(LFPs.data',[15,25],LFPs.fs,3)';
+
+h = hilbert(LFPs.data')';
+
+phase = angle(h);
+
+bad = [1,3,18,67,20,22,15,29,31,95];
+
+grid = [];
+for chn = 1:size(phase,1)
+    [c,r,~] = GetWadeChannelPosition(chn);
+    if(ismember(chn,bad))
+        grid(r,c,:) = nan(1,1,size(phase,2));
+    else
+        grid(r,c,:) = phase(chn,:);
+    end
+end
+grid([1,1,10,10],[1,10,1,10],:) = nan;
+
+vidfile = VideoWriter(fullfile('F:\S\Packets\Video',[blockname,'_PhaseGrad2.mp4']),'MPEG-4');
+open(vidfile);
+
+figure; colormap hsv; clims = [-pi,pi]; 
+for i = 6000:1:7000
+    imagesc(grid(:,:,i),clims); axis off;
+    [fx,fy] = phaseGradient(grid(:,:,i),2);
+    hold on; quiver(1:10,1:10,fx,fy,'color','k','linewidth',1,'autoscalefactor',0.5);%,'autoscale','off');
+%     hold on; contour(1:10,1:10,grid(:,:,i));
+    x = [1,1,6,6]; y = [1,6,1,6];
+    
+    % generalize matrix widths? 
+    quadx = mat2cell(fx,[5,5],[5,5]);
+    quadx = cellfun(@(x) nanmean(x(:)),quadx);
+    quady = mat2cell(fy,[5,5],[5,5]);
+    quady = cellfun(@(x) nanmean(x(:)),quady);
+
+    quiver([3,8],[3,8],quadx,quady,'color','w','linewidth',5,'autoscalefactor',0.5);
+    
+    colorbar;
+    hold off;
+    title(i/fs);
+
+    drawnow
+    
+    F = getframe(gcf);
+    writeVideo(vidfile,F);
+    
+end
+
+close(vidfile);
+
+
